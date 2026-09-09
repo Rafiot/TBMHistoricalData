c1cbde94fde7e7f18c3522962ec9cefa4aa373b0
server <autoupdate@local>
2026-09-09 23:19:36 +0000

    skip commit is no changes

--- process.py
+++ process.py
@@ -53,6 +53,8 @@ def run(stop_id):
 
 def update_repo() -> None:
     repo = Repo('./')
+    if not repo.is_dirty():
+        return
     to_add = ['data']
     repo.index.add(to_add)
     repo.index.commit(f"Add data up to {datetime.now()}")
